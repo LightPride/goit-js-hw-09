be839568471439920692886febbeafbd1d86ce63
LightPride <vlad.grinevich1@gmail.com>
2023-04-10 01:13:41 +0300

    bug fixed

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -14,8 +14,8 @@ function onFormSubmit(event) {
   const step = Number(event.target.elements.step.value);
   const amount = Number(event.target.elements.amount.value);
 
-  for (let i = 1; i <= amount; i += 1) {
-    createPromise(i, delay + i * step)
+  setTimeout(() => {
+    createPromise(1, delay)
       .then(({ position, delay }) => {
         Notiflix.Notify.success(
           `✅ Fulfilled promise ${position} in ${delay}ms`
@@ -26,7 +26,21 @@ function onFormSubmit(event) {
           `❌ Rejected promise ${position} in ${delay}ms`
         );
       });
-  }
+
+    for (let i = 2; i <= amount; i += 1) {
+      createPromise(i, delay + (i - 1) * step)
+        .then(({ position, delay }) => {
+          Notiflix.Notify.success(
+            `✅ Fulfilled promise ${position} in ${delay}ms`
+          );
+        })
+        .catch(({ position, delay }) => {
+          Notiflix.Notify.failure(
+            `❌ Rejected promise ${position} in ${delay}ms`
+          );
+        });
+    }
+  }, delay);
   
 }
 
